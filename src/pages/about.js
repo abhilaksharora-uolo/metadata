fe17f111bb43eb20265f93c8e1e4689d19f07464
abhilaksharora-uolo <abhilaksh.arora@uolo.com>
2024-08-20 12:25:02 +0530

    init

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import React from "react";
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Page Title</title>
+        <meta name="description" content="This is a description of the page." />
+        <meta name="keywords" content="Next.js, SEO, React" />
+        <meta name="author" content="Your Name" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div>about</div>
+    </>
+  );
+};
+
+export default about;
